738eff96e76d3ed6d4101b626c0f57e48f004577
Bruno Silva <bruno3dcontato@gmail.com>
2019-10-03 11:08:34 -0300

    Create init.bat

---
+++ init.bat
@@ -0,0 +1,8 @@
+@echo off
+
+title Inicializador de aplicativos em Node.js
+echo Seja bem-vindo ao inicializador de aplicativos em Node.js!
+echo Para inicializar o script index.js pressione qualquer tecla...
+pause
+
+node index.js
